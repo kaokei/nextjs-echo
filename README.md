5c5efc85b2b2d545fa1556a352b57c7c28d65d92
kaokei <kaokei.rst@gmail.com>
2021-04-12 03:07:34 +0800

    mod: 使用自定义域名

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## 页面地址
 
-请访问 https://echo5.vercel.app/
+请访问 https://echo.kaokei.com/
 
 ## 接口地址
 
-echo 接口地址：https://echo5.vercel.app/api/echo
+echo 接口地址：https://echo.kaokei.com/api/echo
 
 ## 使用方式
 
@@ -14,23 +14,23 @@ echo 接口地址：https://echo5.vercel.app/api/echo
 
 - 不管是 get 还是 post，如果需要自定义 header，那么就需要使用`__echo_headers__`和`__echo_body__`来分别指定 header 数据和 body 数据
 
-- 不管是 get 还是 post，在接口`https://echo5.vercel.app/api/echo`后面是可以任意添加其他 path 的，比如`https://echo5.vercel.app/api/echo/hello`和`https://echo5.vercel.app/api/echo/hello/world`都是有效的接口。
+- 不管是 get 还是 post，在接口`https://echo.kaokei.com/api/echo`后面是可以任意添加其他 path 的，比如`https://echo.kaokei.com/api/echo/hello`和`https://echo.kaokei.com/api/echo/hello/world`都是有效的接口。
 
 #### get 方式一
 
-https://echo5.vercel.app/api/echo?__echo_data__=%7B%22name%22%3A%22zhangsan%22%7D
+https://echo.kaokei.com/api/echo?__echo_data__=%7B%22name%22%3A%22zhangsan%22%7D
 
 该接口返回值是 `{"name": "zhangsan"}`
 
 #### get 方式二
 
-https://echo5.vercel.app/api/echo?__echo_data__=%7B%22__echo_body__%22%3A%7B%22name%22%3A%22zhangsan%22%7D%2C%22__echo_headers__%22%3A%7B%22token%22%3A123%7D%7D
+https://echo.kaokei.com/api/echo?__echo_data__=%7B%22__echo_body__%22%3A%7B%22name%22%3A%22zhangsan%22%7D%2C%22__echo_headers__%22%3A%7B%22token%22%3A123%7D%7D
 
 该接口返回值是 `{"name": "zhangsan"}`，同时会在 response header 中设置`token=123`
 
 #### post 方式一
 
-https://echo5.vercel.app/api/echo
+https://echo.kaokei.com/api/echo
 post 数据如下：
 
 ```json
@@ -43,7 +43,7 @@ post 数据如下：
 
 #### post 方式二
 
-https://echo5.vercel.app/api/echo
+https://echo.kaokei.com/api/echo
 
 post 数据如下：
 
--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,31 @@ const htmlStr = `
 
 <h2>页面地址</h2>
 
-<p>请访问 https://echo5.vercel.app/</p>
+<p>请访问 https://echo.kaokei.com/</p>
 
 <h2>接口地址</h2>
 
-<p>echo 接口地址：https://echo5.vercel.app/api/echo</p>
+<p>echo 接口地址：https://echo.kaokei.com/api/echo</p>
 
 <h2>使用方式</h2>
 
-<ul><li><p>需要注意 get 请求中使用的常量值<code>__echo_data__</code>来指定返回数据，且该数据需要 encodeURIComponent 转码</p></li><li><p>不管是 get 还是 post，如果需要自定义 header，那么就需要使用<code>__echo_headers__</code>和<code>__echo_body__</code>来分别指定 header 数据和 body 数据</p></li><li><p>不管是 get 还是 post，在接口<code>https://echo5.vercel.app/api/echo</code>后面是可以任意添加其他 path 的，比如<code>https://echo5.vercel.app/api/echo/hello</code>和<code>https://echo5.vercel.app/api/echo/hello/world</code>都是有效的接口。</p></li></ul>
+<ul><li><p>需要注意 get 请求中使用的常量值<code>__echo_data__</code>来指定返回数据，且该数据需要 encodeURIComponent 转码</p></li><li><p>不管是 get 还是 post，如果需要自定义 header，那么就需要使用<code>__echo_headers__</code>和<code>__echo_body__</code>来分别指定 header 数据和 body 数据</p></li><li><p>不管是 get 还是 post，在接口<code>https://echo.kaokei.com/api/echo</code>后面是可以任意添加其他 path 的，比如<code>https://echo.kaokei.com/api/echo/hello</code>和<code>https://echo.kaokei.com/api/echo/hello/world</code>都是有效的接口。</p></li></ul>
 
 <h4>get 方式一</h4>
 
-<p>https://echo5.vercel.app/api/echo?__echo_data__=%7B%22name%22%3A%22zhangsan%22%7D</p>
+<p>https://echo.kaokei.com/api/echo?__echo_data__=%7B%22name%22%3A%22zhangsan%22%7D</p>
 
 <p>该接口返回值是 <code>{&quot;name&quot;: &quot;zhangsan&quot;}</code></p>
 
 <h4>get 方式二</h4>
 
-<p>https://echo5.vercel.app/api/echo?__echo_data__=%7B%22__echo_body__%22%3A%7B%22name%22%3A%22zhangsan%22%7D%2C%22__echo_headers__%22%3A%7B%22token%22%3A123%7D%7D</p>
+<p>https://echo.kaokei.com/api/echo?__echo_data__=%7B%22__echo_body__%22%3A%7B%22name%22%3A%22zhangsan%22%7D%2C%22__echo_headers__%22%3A%7B%22token%22%3A123%7D%7D</p>
 
 <p>该接口返回值是 <code>{&quot;name&quot;: &quot;zhangsan&quot;}</code>，同时会在 response header 中设置<code>token=123</code></p>
 
 <h4>post 方式一</h4>
 
-<p>https://echo5.vercel.app/api/echo
+<p>https://echo.kaokei.com/api/echo
 post 数据如下：</p>
 
 <p><code>json
@@ -40,7 +40,7 @@ post 数据如下：</p>
 
 <h4>post 方式二</h4>
 
-<p>https://echo5.vercel.app/api/echo</p>
+<p>https://echo.kaokei.com/api/echo</p>
 
 <p>post 数据如下：</p>
 
